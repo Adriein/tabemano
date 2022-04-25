873343c4f93c4acf0f1c924c04ca8956060c3af7
Adriein <adria.claret@gmail.com>
2022-04-25 20:35:18 +0200

    creating pricing use case

--- server/src/Backoffice/Shared/Domain/Vo/PricingVo.ts
+++ server/src/Backoffice/Shared/Domain/Vo/PricingVo.ts
@@ -1,9 +1,17 @@
+import { ID } from "Shared/Domain/Vo/Id.vo";
+
 export class PricingVo {
   constructor(
+    private readonly _id: ID,
     private readonly _name: string,
     private readonly _price: number,
     private readonly _duration: number
   ) {}
+
+
+  public get id(): ID {
+    return this._id;
+  }
 
   public get name(): string {
     return this._name;
