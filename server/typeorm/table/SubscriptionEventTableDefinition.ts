02226a86e221550b9f4fac0ac422983bbe896094
Adriein <adria.claret@gmail.com>
2022-06-27 17:22:32 +0200

    typeorm creating migrations

---
+++ server/typeorm/table/SubscriptionEventTableDefinition.ts
@@ -0,0 +1,37 @@
+import { Table } from "typeorm";
+
+export const SubscriptionEventTableDefinition = new Table({
+  name: 'ta_subscription_events',
+  columns: [
+    {
+      name: 'se_id',
+      type: 'varchar',
+      isPrimary: true
+    },
+    {
+      name: 'se_event',
+      type: 'varchar',
+    },
+    {
+      name: 'se_subscription_id',
+      type: 'varchar',
+    },
+    {
+      name: 'se_created_at',
+      type: 'timestamp',
+      precision: 0
+    },
+    {
+      name: 'se_updated_at',
+      type: 'timestamp',
+      precision: 0
+    }
+  ],
+  foreignKeys: [
+    {
+      columnNames: [ 'se_subscription_id' ],
+      referencedColumnNames: [ 'su_id' ],
+      referencedTableName: 'ta_subscription'
+    }
+  ]
+});
