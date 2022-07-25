4651fd8628b8b3ed45989deaca953cce051b5574
Adriein <adria.claret@gmail.com>
2022-07-25 18:36:10 +0200

    adding auth guard

--- server/src/Backoffice/Tenant/Infrastructure/Controller/RegisterClient/RegisterClientController.ts
+++ server/src/Backoffice/Tenant/Infrastructure/Controller/RegisterClient/RegisterClientController.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UseInterceptors } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
 import { RegisterClientCommand } from "Backoffice/Tenant/Application/RegisterClient/RegisterClientCommand";
 import { RegisterClientApiRequest } from "Backoffice/Tenant/Infrastructure/Controller/RegisterClient/RegisterClientApiRequest";
+import { AuthGuard } from "Shared/Infrastructure/Guard/AuthGuard";
+import { UserInterceptor } from "Shared/Infrastructure/Interceptor/UserInterceptor";
 
 @Controller()
 export class RegisterClientController {
   constructor(private readonly commandBus: CommandBus) {}
 
+  @UseInterceptors(UserInterceptor)
+  @UseGuards(AuthGuard)
   @Post('/register/client')
   public async register(
     @Body() body: RegisterClientApiRequest
