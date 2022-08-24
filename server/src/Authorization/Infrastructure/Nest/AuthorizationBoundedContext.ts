f83a8ed2a901f74ab1b9152568eb73e5b3909eb4
laubernal <lbernalrodrguez@gmail.com>
2022-08-24 16:36:27 +0200

    add CheckPermissionController to authorization bounded context

--- server/src/Authorization/Infrastructure/Nest/AuthorizationBoundedContext.ts
+++ server/src/Authorization/Infrastructure/Nest/AuthorizationBoundedContext.ts
@@ -1,22 +1,21 @@
-import { Module } from "@nestjs/common";
-import { CqrsModule } from "@nestjs/cqrs";
-import { CheckPermissionQueryHandler } from "Authorization/Application/CheckPermission/CheckPermissionQueryHandler";
-import { GetPermissionsQueryHandler } from "Authorization/Application/GetPermissions/GetPermissionsQueryHandler";
-import { RegisterTenantCommandHandler } from "Authorization/Application/RegisterTenant/RegisterTenantCommandHandler";
-import { SignInQueryHandler } from "Authorization/Application/SignIn/SignInQueryHandler";
-import { RegisterTenantController } from "Authorization/Infrastructure/Controller/RegisterTenant/RegisterTenantController";
-import { SignInController } from "Authorization/Infrastructure/Controller/SignIn/SignInController";
-import { PgAuthMapper } from "Authorization/Infrastructure/Persistance/Mapper/PgAuthMapper";
-import { PgRoleMapper } from "Authorization/Infrastructure/Persistance/Mapper/PgRoleMapper";
-import { PgAuthRepository } from "Authorization/Infrastructure/Persistance/Repository/PgAuthRepository";
-import { PgRoleRepository } from "Authorization/Infrastructure/Persistance/Repository/PgRoleRepository";
-import { CryptoService } from "Shared/Domain/Services/CryptoService";
-import { TypeOrmModule } from "Shared/Infrastructure/Persistance/TypeOrmModule";
-import { GetPermissionsController } from "../Controller/GetPermissions/GetPermissionsController";
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CheckPermissionQueryHandler } from 'Authorization/Application/CheckPermission/CheckPermissionQueryHandler';
+import { GetPermissionsQueryHandler } from 'Authorization/Application/GetPermissions/GetPermissionsQueryHandler';
+import { RegisterTenantCommandHandler } from 'Authorization/Application/RegisterTenant/RegisterTenantCommandHandler';
+import { SignInQueryHandler } from 'Authorization/Application/SignIn/SignInQueryHandler';
+import { RegisterTenantController } from 'Authorization/Infrastructure/Controller/RegisterTenant/RegisterTenantController';
+import { SignInController } from 'Authorization/Infrastructure/Controller/SignIn/SignInController';
+import { PgAuthMapper } from 'Authorization/Infrastructure/Persistance/Mapper/PgAuthMapper';
+import { PgRoleMapper } from 'Authorization/Infrastructure/Persistance/Mapper/PgRoleMapper';
+import { PgAuthRepository } from 'Authorization/Infrastructure/Persistance/Repository/PgAuthRepository';
+import { PgRoleRepository } from 'Authorization/Infrastructure/Persistance/Repository/PgRoleRepository';
+import { CryptoService } from 'Shared/Domain/Services/CryptoService';
+import { TypeOrmModule } from 'Shared/Infrastructure/Persistance/TypeOrmModule';
+import { CheckPermissionController } from '../Controller/CheckPermission/CheckPermissionController';
+import { GetPermissionsController } from '../Controller/GetPermissions/GetPermissionsController';
 
-const Services = [
-  CryptoService
-];
+const Services = [CryptoService];
 
 const Repositories = [
   {
@@ -32,32 +31,23 @@ const Repositories = [
 const Controllers = [
   SignInController,
   RegisterTenantController,
-  GetPermissionsController
+  GetPermissionsController,
+  CheckPermissionController,
 ];
 
 const Handlers = [
   SignInQueryHandler,
   RegisterTenantCommandHandler,
   GetPermissionsQueryHandler,
-  CheckPermissionQueryHandler
+  CheckPermissionQueryHandler,
 ];
 
-const Mappers = [
-  PgAuthMapper,
-  PgRoleMapper
-];
+const Mappers = [PgAuthMapper, PgRoleMapper];
 
 @Module({
-  imports: [ CqrsModule, TypeOrmModule ],
-  controllers: [
-    ...Controllers
-  ],
-  providers: [
-    ...Repositories,
-    ...Handlers,
-    ...Services,
-    ...Mappers
-  ],
+  imports: [CqrsModule, TypeOrmModule],
+  controllers: [...Controllers],
+  providers: [...Repositories, ...Handlers, ...Services, ...Mappers],
   exports: [],
 })
 export class AuthorizationBoundedContext {}
