fb4f6588569eca75361f20e84a2d086481725187
laubernal <lbernalrodrguez@gmail.com>
2022-08-24 16:19:14 +0200

    implement CheckPermissionController

---
+++ server/src/Authorization/Infrastructure/Controller/CheckPermission/CheckPermissionApiRequest.ts
@@ -0,0 +1,4 @@
+export interface CheckPermissionApiRequest {
+  tenantId: string;
+  moduleId: string;
+}
---
+++ server/src/Authorization/Infrastructure/Controller/CheckPermission/CheckPermissionApiResponse.ts
@@ -0,0 +1,6 @@
+export type CheckPermissionApiResponse = {
+  id: string;
+  tenantId: string;
+  moduleId: string;
+  moduleName: string;
+};
