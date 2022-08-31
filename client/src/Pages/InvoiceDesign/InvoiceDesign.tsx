ebb6d0084e0305812d3a241c94bdd319eb9109ef
Adriein <adria.claret@gmail.com>
2022-08-31 11:53:24 +0200

    adding draggable

--- client/src/Pages/InvoiceDesign/InvoiceDesign.tsx
+++ client/src/Pages/InvoiceDesign/InvoiceDesign.tsx
@@ -1,7 +1,62 @@
+import { DndContext, useDroppable } from '@dnd-kit/core';
 import React from 'react';
+import { Box, MantineTheme } from '@mantine/core';
+import Draggable from "../../Shared/Components/Draggable";
 
 const InvoiceDesign = () => {
-  return <div>invoice design</div>
+  const { setNodeRef } = useDroppable({
+    id: 'invoice-droppable-id',
+  });
+  const handleDragEnd = (event: any) => {
+    console.log(event)
+    if (event.over && event.over.id === 'invoice-droppable-id') {
+      console.log('aaaaa')
+    }
+  }
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <Box
+        sx={(theme: MantineTheme) => ({
+          display: 'flex'
+        })}
+      >
+        <Box
+          sx={(theme: MantineTheme) => ({
+            flexGrow: 1
+          })}
+
+        >
+          <Box
+            ref={setNodeRef}
+            sx={(theme: MantineTheme) => ({
+              backgroundColor: theme.colors.gray[0],
+              borderRadius: theme.radius.md,
+              cursor: 'pointer',
+              height: window.innerHeight - 92,
+              width: '800px',
+            })}
+          >
+          </Box>
+        </Box>
+        <Box
+          sx={(theme: MantineTheme) => ({
+            backgroundColor: theme.colors.gray[0],
+            borderRadius: theme.radius.md,
+            cursor: 'pointer',
+            height: window.innerHeight - 92,
+            width: '300px',
+            marginLeft: '20px'
+          })}
+        >
+          <Draggable>
+            <div>draggable item</div>
+          </Draggable>
+        </Box>
+      </Box>
+    </DndContext>
+
+
+  );
 }
 
 export default InvoiceDesign;
---
+++ client/src/Pages/InvoiceDesign/Styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const DesignTable = styled.div`
+  display: flex,
+`;
