46697bef9cfc635411e705e67976134b6ed7ad73
Adriein <adria.claret@gmail.com>
2022-09-24 12:17:54 +0200

    creating notification module

--- server/src/Backoffice/Notification/Infrastructure/Service/SendGridSmtpService.ts
+++ server/src/Backoffice/Notification/Infrastructure/Service/SendGridSmtpService.ts
@@ -1,7 +1,12 @@
 import { Email } from "Backoffice/Notification/Domain/Entity/Email";
 import { ISmtpService } from "Backoffice/Notification/Domain/Service/ISmtpService";
+import sgMail, { MailDataRequired } from '@sendgrid/mail';
 
 export class SendGridSmtpService implements ISmtpService {
+  constructor() {
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY!);
+  }
+
   public async send(email: Email): Promise<void> {
     return Promise.resolve(undefined);
   }
