6a9b3e9a17be721e1ef584a98c6d7565b32e5421
Adriein <adria.claret@gmail.com>
2022-10-02 17:46:50 +0200

    fixed db seeding scripts

--- server/src/Backoffice/Notification/Infrastructure/Nest/NotificationModule.ts
+++ server/src/Backoffice/Notification/Infrastructure/Nest/NotificationModule.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { SendAboutToExpireSubscriptionEmailHandler } from "Backoffice/Notification/Application/SendAboutToExpireSubscriptionEmail/SendAboutToExpireSubscriptionEmailHandler";
+import { TenantMapper } from "Backoffice/Notification/Infrastructure/Persistance/Mapper/TenantMapper";
 import { PgClientRepository } from "Backoffice/Notification/Infrastructure/Persistance/Repository/PgClientRepository";
 import { SendGridSmtpService } from "Backoffice/Notification/Infrastructure/Service/SendGridSmtpService";
 import { TypeOrmModule } from "Shared/Infrastructure/Persistance/TypeOrmModule";
@@ -23,13 +24,18 @@ const Service = [
   }
 ];
 
+const Mapper = [
+  TenantMapper
+];
+
 @Module({
   imports: [ CqrsModule, TypeOrmModule ],
   controllers: [],
   providers: [
     ...Handlers,
     ...Service,
-    ...Repository
+    ...Repository,
+    ...Mapper
   ],
   exports: [],
 })
---
+++ server/src/Backoffice/Shared/Domain/Tenant/TenantFilter.ts
@@ -0,0 +1,102 @@
+import { Filter } from "Shared/Domain/Entities/Filter";
+import { DateVo } from "Shared/Domain/Vo/Date.vo";
+import { Email } from "Shared/Domain/Vo/Email.vo";
+import { ID } from "Shared/Domain/Vo/Id.vo";
+import { RoleType } from "Shared/Domain/Vo/RoleType";
+
+export class TenantFilter extends Filter {
+  public static ID_FILTER = 'id';
+  public static TENANT_ID_FILTER = 'tenantId';
+  public static EMAIL_FILTER = 'email';
+  public static ROLE_FILTER = 'roleType';
+  public static ACTIVE_FILTER = 'isActive';
+  public static CONFIG_ALLOW_SEND_WARNINGS_FILTER = 'sendWarnings';
+  public static CONFIG_ALLOW_SEND_NOTIFICATIONS_FILTER = 'sendNotifications';
+  public static SUBSCRIPTION_PRICING_NAME_FILTER = 'pricingName';
+  public static SUBSCRIPTION_DURATION_FILTER = 'pricingDuration';
+  public static SUBSCRIPTION_AMOUNT_FILTER = 'pricingAmount';
+  public static SUBSCRIPTION_VALID_TO_FILTER = 'validTo';
+  public static SUBSCRIPTION_PAYMENT_DATE_FILTER = 'paymentDate';
+  public static SUBSCRIPTION_IS_ACTIVE_FILTER = 'subscriptionIsActive';
+  public static SUBSCRIPTION_IS_EXPIRED_FILTER = 'subscriptionIsExpired';
+
+  public static create(): TenantFilter {
+    return new TenantFilter();
+  }
+
+  protected data: Map<string, any> = new Map();
+
+  public withId(id: ID): this {
+    this.data.set(TenantFilter.ID_FILTER, id);
+    return this;
+  }
+
+  public withTenantId(id: ID): this {
+    this.data.set(TenantFilter.TENANT_ID_FILTER, id);
+    return this;
+  }
+
+  public withEmail(email: Email): this {
+    this.data.set(TenantFilter.EMAIL_FILTER, email);
+    return this;
+  };
+
+  public withRole(roleType: RoleType): this {
+    this.data.set(TenantFilter.ROLE_FILTER, roleType);
+    return this;
+  }
+
+  public isActive(isActive: boolean): this {
+    this.data.set(TenantFilter.ACTIVE_FILTER, isActive);
+    return this;
+  }
+
+  public withPricingName(name: string): this {
+    this.data.set(TenantFilter.SUBSCRIPTION_PRICING_NAME_FILTER, name);
+    return this;
+  }
+
+  public withSubscriptionDuration(duration: number): this {
+    this.data.set(TenantFilter.SUBSCRIPTION_DURATION_FILTER, duration);
+    return this;
+  }
+
+  public withSubscriptionPrice(amount: number): this {
+    this.data.set(TenantFilter.SUBSCRIPTION_AMOUNT_FILTER, amount);
+    return this;
+  }
+
+  public withAllowSendWarnings(allow: boolean) {
+    this.data.set(TenantFilter.CONFIG_ALLOW_SEND_WARNINGS_FILTER, allow);
+    return this;
+  }
+
+  public withAllowSendNotifications(allow: boolean) {
+    this.data.set(TenantFilter.CONFIG_ALLOW_SEND_NOTIFICATIONS_FILTER, allow);
+    return this;
+  }
+
+  public withSubscriptionValidTo(validTo: DateVo) {
+    this.data.set(TenantFilter.SUBSCRIPTION_VALID_TO_FILTER, validTo);
+    return this;
+  }
+
+  public withSubscriptionPaymentOn(paymentDate: DateVo) {
+    this.data.set(TenantFilter.SUBSCRIPTION_PAYMENT_DATE_FILTER, paymentDate);
+    return this;
+  }
+
+  public withSubscriptionActive(isActive: boolean) {
+    this.data.set(TenantFilter.SUBSCRIPTION_IS_ACTIVE_FILTER, isActive);
+    return this;
+  }
+
+  public withSubscriptionExpired(isExpired: boolean) {
+    this.data.set(TenantFilter.SUBSCRIPTION_IS_EXPIRED_FILTER, isExpired);
+    return this;
+  }
+
+  public apply(): Map<string, any> {
+    return this.data;
+  }
+}
